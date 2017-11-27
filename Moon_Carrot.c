efda2ec116c01c04420e109df6de3b09532b91c7
drodevilosun <drodevilosun@gmail.com>
2017-11-27 21:24:01 +0700

    Update Moon_Carrot.c

--- Moon_Carrot.c
+++ Moon_Carrot.c
@@ -1,2 +1,22 @@
-In chapter 886, Carrot said to Bropper that the day in the story day will be a moon 
-
+In chapter 886, Carrot said to Bropper that the day in the story day will be a moon
+The details of the moon that Carrot said isn't new point
+The moon have been mentioned before
+But Oda didn't explain more details about the moon in story, it may imply for the strength of Minks clan.
+In chapter 805, Luffy accidentally strayed into the forest and conflicted with Mink guard team.
+Wanda was stopping the fight and talked with Luffy that "You should be thankful it isn't dark yet...(moon)"
+In chapter 811, Wanda was remind about the "moon" one more time.
+But in this time, Wanda talked "Tonight a full moon. But it appears to be hidden by the clouds. We're lucky". What  is "lucky" mean? 
+When talk about the moon, Wanda seem very careful and worry about that.
+This means something wrong, danger and unusual with Mink clan while the moon appearing.
+In the real world, the animals still be affected by "the moon"
+Some animals most active at night and especially when the moon is bright.
+It makes them stronger and more rampage.
+As you know, the most animal in Mink clans stand by 2 feet as human
+A beast as Zoan-Types, the same with fishmen clan.
+So, Wanda may aim about this characteristic.
+No doubt anymore, Eiichiro Oda was applying animal characteristics in the real world into Mink clan.
+Mink clan is the clan that anyone are warrior even a child and have ability used electric.
+Hence, it may "At the moon day, Mink clan more more rampage and scarier than ever they used to be" (May be out of control and don't know who they are and even their friend?) 
+Strength, speed and physically of Mink clan will be increased to maximum
+In the other words, they will be strongest while the moon brighting.
+May be author will disclose the secret in chapter 888 or chapter 889. :D                                         
