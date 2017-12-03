6c6b831528d1db74aa38db179f9c884e385f0dbb
drodevilosun <drodevilosun@gmail.com>
2017-12-03 23:17:48 +0700

    Update BlackBeard.c

--- BlackBeard.c
+++ BlackBeard.c
@@ -1,2 +1,2 @@
 https://www.youtube.com/watch?v=lAptRNHbhDo
-https://www.youtube.com/watch?v=Kn1XdqlznmU first time Luffy meet Blackbeard: 145-150 
+https://www.youtube.com/watch?v=Kn1XdqlznmU first time Luffy meet Blackbeard: 146
