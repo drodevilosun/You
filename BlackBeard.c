a7dbb2e5a68ef3e62bd5fc7e77f51c2c9e7d2933
drodevilosun <drodevilosun@gmail.com>
2017-12-04 23:52:20 +0700

    Update BlackBeard.c

--- BlackBeard.c
+++ BlackBeard.c
@@ -1,2 +1,6 @@
 https://www.youtube.com/watch?v=lAptRNHbhDo
 https://www.youtube.com/watch?v=Kn1XdqlznmU first time Luffy meet Blackbeard: 146-147
+
+Blackbeard - The Wise man or Coward
+‘Malignant And Psychopathic’ Blackbeard?
+Disorders of mood and Schizophrenia of Blackbeard
