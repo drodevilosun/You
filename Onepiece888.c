0a07851971b15cd4864834ca6637bfa3b2133d3a
drodevilosun <drodevilosun@gmail.com>
2017-12-10 18:36:19 +0700

    Update Onepiece888.c

--- Onepiece888.c
+++ Onepiece888.c
@@ -1,1 +1,10 @@
-
+So, Ideo has 6 cover story. In this week, Ideo made a ship for him and created pirate crew with his name.
+Finally, Ideo don't need both Longarm Tribe and Longleg Tribe. He merged them ship into his ship, robbed them property and let them in abandon island to resolve conflict.
+Jolly Roger of the crew represent for Ideo arm and the XXX symbol in skullcap represent for old name "Gym Martial Arts Alliance"
+Luffy has came back to the mirror world and fight with Katakuri for a time. We didn't see Brulee, maybe she hide in outside.
+When Big Mom didn't awake, the Homies which contained the soul were reduced morale and easy to be cheated
+Every battle ship of Big Mom is the same, the main ship which owned by Big Mom's son is bigger and sharper than the others. Especially, the prow represent for the captain's characteristic of that ship.
+For example: Bavarois owned a ship which the prow had a hat the same with his hat.
+Bavarois was introduced in this chapter even Arc in final state. He may has a roles in next chapter (1 - be beaten, 2 - use a special power =]])
+The helmsman is very important when go out to the sea, Jinbe - a fishmen who has knowledge about ocean currents. So, he can drive the ship very fast and leave Big Mom's legions behind.
+  
