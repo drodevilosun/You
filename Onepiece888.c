e03ff9249087f6637c7dc4c9bd87594bbb0e4600
drodevilosun <drodevilosun@gmail.com>
2017-12-11 21:45:35 +0700

    Update Onepiece888.c

--- Onepiece888.c
+++ Onepiece888.c
@@ -1,13 +1,40 @@
 So, Ideo has 6 cover story. In this week, Ideo made a ship for him and created pirate crew with his name.
+  
 Finally, Ideo don't need both Longarm Tribe and Longleg Tribe. He merged them ship into his ship, robbed them property and let them in abandon island to resolve conflict.
+  
 Jolly Roger of the crew represent for Ideo arm and the XXX symbol in skullcap represent for old name "Gym Martial Arts Alliance"
+  
 Luffy has came back to the mirror world and fight with Katakuri for a time. We didn't see Brulee, maybe she hide in outside.
+  
 When Big Mom didn't awake, the Homies which contained the soul were reduced morale and easy to be cheated
+  
 Every battle ship of Big Mom is the same, the main ship which owned by Big Mom's son is bigger and sharper than the others. Especially, the prow represent for the captain's characteristic of that ship.
+  
 For example: Bavarois owned a ship which the prow had a hat the same with his hat.
+  
 Bavarois was introduced in this chapter even Arc in final state. He may has a roles in next chapter (1 - be beaten, 2 - use a special power =]])
+  
 The helmsman is very important when go out to the sea, Jinbe - a fishmen who has knowledge about ocean currents. So, he can drive the ship very fast and leave Big Mom's legions behind.
 2 girls stay beside Smoothie was appeared in the last chapter
+
 Fasting and walking for a long time, Big Mom was skinny. I guess Big Mom will be fat again after eat Sanji's cake.
+  
 Daifuku looks awake and he decide to use normal cannons instead Homie cannons which were reduced morale.
+  
 Carrot's new shape name "Ceylon" (Suron - it means: lion moon). Ceylon, more widely known as Siri Lanka, it is a kind of lion was extinct before human culture appears about 37.000 years B.C.
+
+  In the day full moon ( it happen one time each month), Mink clan can transfer to a new body which faster and stronger than before. However, not everybody on Mink clan can be tranfered to new body. If some one on Mnk clan want to reach "Ceylon" power,  they must be trained very hard (Carrot was trained by Pedro)
+  
+  After transform, Carrot howl loudly as the the wolf howl in the night has full moon
+  
+  We can see Carrot has the long white hair, long tail feathers, red eye, sexy body and the lightning around Carrot's body. (The same with super saiyan :D)
+  
+ Nami, Brook and Chopper was surprised and scared when saw Carrot transform.
+  
+"Ceylon" power of Mink clan is very famous. Jinbe, Daifuku or even the sailor in the ship know still know Mink clan power.
+ 
+Every ship of Big Mom's son has the name in the sail.
+  
+We could see that power is a strong power and characteristics of Mink clan. They can fly very easy while everyone moved with the ship. So Carrot can easy attack the enemy and protect her ally.
+  
+How serious is Chopper's emotion.
